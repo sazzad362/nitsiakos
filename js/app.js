40c9dd0fce5daf94a14cebe5515209b416057b38
BadhonKhan01 <badhon.officialmail@gmail.com>
2019-08-18 03:24:18 +0600

    create home page only desktop

--- js/app.js
+++ js/app.js
@@ -8,6 +8,43 @@
    *----------------------------------
    */
 	$(document).ready(function() {
+
+		$('#owl_service_slider').owlCarousel({
+		    loop:true,
+		    margin:0,
+		    nav:true,
+		    dots: false,
+	    	navText: ['<span class="fa fa-angle-left"></span>','<span class="fa fa-angle-right"></span>'],
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:3
+		        },
+		        1000:{
+		            items:3
+		        }
+		    }
+		})
+
+		$('#instagram_slider_img').owlCarousel({
+		    loop:true,
+		    margin:0,
+		    nav:false,
+		    dots: true,
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:3
+		        },
+		        1000:{
+		            items:5
+		        }
+		    }
+		})
 		
 	});// DOM Ready
 
