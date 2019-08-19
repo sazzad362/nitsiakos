b79060f902143b2bca01a115e6e06c2bdc70d7ea
BadhonKhan01 <badhon.officialmail@gmail.com>
2019-08-19 17:14:04 +0600

    complete product html

--- js/app.js
+++ js/app.js
@@ -45,6 +45,24 @@
 		        }
 		    }
 		})
+
+
+		$('#product_slider_content').owlCarousel({
+		    loop:true,
+		    margin:10,
+		    nav:true,
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:3
+		        },
+		        1000:{
+		            items:4
+		        }
+		    }
+		})
 		
 	});// DOM Ready
 
